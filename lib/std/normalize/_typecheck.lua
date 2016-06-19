e543f7134a8d6fc6cb00528f15f8d82424ccae3d
Gary V. Vaughan <gary@gnu.org>
2016-06-19 15:31:08 -0700

    Revert "_typecheck: distinguish functor from plain table in argcheck diagnostics."
    We don't need to be fussing with functor detection here!
    
    This reverts commit dd2435f4e74969f1e1a4abc6714d3ca74f2bcc76.

--- lib/std/normalize/_typecheck.lua
+++ lib/std/normalize/_typecheck.lua
@@ -147,12 +147,8 @@ end
 local function fail (expected, argu, i, got)
   if i > argu.n then
     got = "no value"
-  elseif got == nil then
-    if getmetamethod (argu[i], "__call") then
-      got = "functor"
-    else
-      got = type (argu[i])
-    end
+  else
+    got = got or type (argu[i])
   end
   return "got " .. got, expected
 end
